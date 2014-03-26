d1a8cbf8fc7284befd9049d95538456e2eeb9c30
Christophe Quignon <cuignon@gmail.com>
2014-03-27 00:03:41 +0100

    Exercise 2 without prime calculation

---
+++ 2_Ex_Quignon/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.LinkedList;
+
+
+public class Main {
+	public static void main(String[] args) {
+		int largestInteger =Integer.MAX_VALUE; 
+		long largestLong =Long.MAX_VALUE;
+		
+		//subtask 1
+		List<Integer> integerPrimeList = new LinkedList<Integer>();
+		integerPrimeList = allprimes(largestInteger);
+		
+		List<Long> longPrimeList=new LinkedList<Long>();
+		longPrimeList = allprimes(largestLong);
+		
+		
+		
+		//subtask 2
+		
+		
+		//subtask 3
+		
+	}
+
+	private static List<Long> allprimes(Long n) {
+		// TODO
+		return null;
+	}
+
+	private static List<Integer> allprimes(Integer n){
+		//TODO
+		return null;
+	}
+}
